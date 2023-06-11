8aff23050eb71dc08e3b07a86c59f0106e028348
Shakib AL Nur <shakibalnur3268@gmail.com>
2023-06-12 02:14:16 +0600

    admin

--- src/pages/Dashboard/managedUser/Manageduser.jsx
+++ src/pages/Dashboard/managedUser/Manageduser.jsx
@@ -1,12 +1,32 @@
 import { useQuery } from '@tanstack/react-query';
 import React from 'react';
 import { Helmet } from 'react-helmet-async';
+import Swal from 'sweetalert2';
 
 const Manageduser = () => {
     const {data : users = [],refetch} = useQuery(['users'],async () =>{
         const res = await fetch('http://localhost:5000/users')
         return res.json()
     })
+
+    const handleMakeAdmin = user =>{
+        fetch(`http://localhost:5000/users/admin/${user._id}`,{
+            method: 'PATCH'
+        })
+        .then(res => res.json())
+        .then(data => {
+            if(data.modifiedCount){
+                refetch()
+                Swal.fire({
+                    position: 'top-end',
+                    icon: 'success',
+                    title: `${user.name} is an Admin`,
+                    showConfirmButton: false,
+                    timer: 1500
+                  })
+            }
+        })
+    }
     return (
         <div>
             <Helmet>
@@ -22,7 +42,7 @@ const Manageduser = () => {
         <th>Name</th>
         <th>Email</th>
         <th>Admin</th>
-        <th>Instructor</th>
+        <th>Istructor</th>
       </tr>
     </thead>
     <tbody>
@@ -31,7 +51,7 @@ const Manageduser = () => {
             <th>{index+1}</th>
             <td>{user.name}</td>
             <td>{user.email}</td>
-            <td>{user.role === 'admin' ? 'admin' : <button className='btn btn-ghost bg-orange-500'>Make Admin</button>}</td>
+            <td>{user.role === 'admin' ? 'admin' : <button className='btn btn-ghost bg-orange-500' onClick={() => handleMakeAdmin(user)}>Make Admin</button>}</td>
             <td>{user.role === 'instructor' ? 'instructor' : <button className='btn btn-ghost bg-blue-500'>Make instructor</button>}</td>
           </tr>)
       }
